4d191139aeefaac58b056cba27881c406cf57e1b
Moien <137896500+moienheydari@users.noreply.github.com>
2024-03-29 03:43:36 +0100

    contact us text

--- src/Components/ContactUS.jsx
+++ src/Components/ContactUS.jsx
@@ -4,7 +4,10 @@ import '../css/ContactUS.css';
 export default function ContactUS() {
 
     return (
-        <>
-        </>
+        <div className='contactus'>
+            <p className='title'>CONTACT US</p>
+            <p className='expl'>For any questions, information or needs, you can contact us by email at icarus.flyteam@gmail.com
+            or on the following social channels :</p>
+        </div>
     )
 }
