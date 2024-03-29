3b67487d5af510a792da9be85a7c1751538a157d
Moien <137896500+moienheydari@users.noreply.github.com>
2024-03-29 03:39:34 +0100

    added to projects

--- src/Components/Introduce.jsx
+++ src/Components/Introduce.jsx
@@ -4,7 +4,10 @@ import '../css/Introduce.css';
 export default function Introduce() {
 
     return (
-        <>
-        </>
+        <div className='introduce'>
+            <p className='title1'>Team ICARUS PoliTO</p>
+            <p className='title2'>WHO WE ARE?</p>
+            <p className='expl'>ICARUS PoliTO is a Student Team of the Politecnico di Torino. Born in 2015 with a purely aerospace character, the Team has more than 90 effective members working on 4 main projects</p>
+        </div>
     )
 }
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -9,12 +9,20 @@ export default function Projects() {
 
     return (
         <div className='projects'>
-            <>
-            </>
+            <div className='expl'>
+                Our PROJECTS :
+                1. ACC: Design and Construction of a Competition UAV
+
+                2. RA: Design and Construction of a Solar-Powered UAV
+
+                3. DART: Design and Construction of a Rocket Model with Solid Propellant
+
+                4. IT: Dealing with the Electronic and IT part of the projects, the Communication and Design Area of the Team in internal management and communication, and Taking Care of Social Media.
+            </div>
 
             <div className='gallerycontainer'>
                 <div className='box box-1' style={{ '--img': `url(${myImage1})` }} data-text="ACC"></div>
-                <div className='box box-2' style={{ '--img': `url(${myImage2})` }} data-text="Dart"></div>
+                <div className='box box-2' style={{ '--img': `url(${myImage2})` }} data-text="DART"></div>
                 <div className='box box-3' style={{ '--img': `url(${myImage3})` }} data-text="RA"></div>
                 <div className='box box-4' style={{ '--img': `url(${myImage4})` }} data-text="IT"></div>
             </div>
