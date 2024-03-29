ba25b3bbbc066d9b31ae14005ffd24d3d2837d8e
Moien <137896500+moienheydari@users.noreply.github.com>
2024-03-29 02:53:48 +0100

    logo set

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../css/Logo.css';
+import myImage from '../Assests/ICARUS_LOGO_BLACK.png';
 
 export default function Logo() {
 
     return (
-        <>
-        </>
+        <div className='logo'>
+            <img src={myImage} alt="Logo" />
+        </div>
     )
 }
--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import '.../css/Introduce.css';
+import '../../css/Home.css';
+import Logo from '../Logo.jsx';
+import NavBar from '../NavBar.jsx';
+import Introduce from '../Introduce.jsx';
+import Projects from '../Projects.jsx';
+import ContactUS from '../ContactUS';
 
 export default function Home() {
 
     return (
-        <>
-        </>
+        <div className='home'>
+            <div className='header'>
+                <Logo />
+                <NavBar />
+            </div>
+            <Introduce />
+            <Projects />
+            <ContactUS />
+        </div>
     )
 }
