adbd6c227bd846c3e9938cdcc85063bb1705ed6b
Moien <137896500+moienheydari@users.noreply.github.com>
2024-04-05 22:30:57 +0200

    image import fixed

--- src/Components/HorizontalGallery.jsx
+++ src/Components/HorizontalGallery.jsx
@@ -32,11 +32,11 @@ export default function HorizontalGallery() {
 
     return (
         <div className='horizontalgallery' style={{ transform: `translateY(${-height + 'px'})`, height: `${height * 4}px` }}>
-            <div className='slide slide-1' style={{ '--img': `url(${myImage1})`, transform: `translateY(${slides[0][0] + 'px'})`, opacity: slides[0][1], transition: `all ${delay - 300}ms`, height: `${height}px` }}></div>
-            <div className='slide slide-2' style={{ '--img': `url(${myImage2})`, transform: `translateY(${slides[1][0] + 'px'})`, opacity: slides[1][1], transition: `all ${delay - 300}ms`, height: `${height}px` }}></div>
-            <div className='slide slide-3' style={{ '--img': `url(${myImage3})`, transform: `translateY(${slides[2][0] + 'px'})`, opacity: slides[2][1], transition: `all ${delay - 300}ms`, height: `${height}px` }}></div>
-            <div className='slide slide-4' style={{ '--img': `url(${myImage4})`, transform: `translateY(${slides[3][0] + 'px'})`, opacity: slides[3][1], transition: `all ${delay - 300}ms`, height: `${height}px` }}></div>
-            <div className='slide slide-5' style={{ '--img': `url(${myImage5})`, transform: `translateY(${slides[4][0] + 'px'})`, opacity: slides[4][1], transition: `all ${delay - 300}ms`, height: `${height}px` }}></div>
+            <div className='slide slide-1' style={{ background: `url(${myImage1}) center center`, transform: `translateY(${slides[0][0] + 'px'})`, opacity: slides[0][1], transition: `all ${delay - 300}ms`, height: `${height}px` }}></div>
+            <div className='slide slide-2' style={{ background: `url(${myImage2}) center center`, transform: `translateY(${slides[1][0] + 'px'})`, opacity: slides[1][1], transition: `all ${delay - 300}ms`, height: `${height}px` }}></div>
+            <div className='slide slide-3' style={{ background: `url(${myImage3}) center center`, transform: `translateY(${slides[2][0] + 'px'})`, opacity: slides[2][1], transition: `all ${delay - 300}ms`, height: `${height}px` }}></div>
+            <div className='slide slide-4' style={{ background: `url(${myImage4}) center center`, transform: `translateY(${slides[3][0] + 'px'})`, opacity: slides[3][1], transition: `all ${delay - 300}ms`, height: `${height}px` }}></div>
+            <div className='slide slide-5' style={{ background: `url(${myImage5}) center center`, transform: `translateY(${slides[4][0] + 'px'})`, opacity: slides[4][1], transition: `all ${delay - 300}ms`, height: `${height}px` }}></div>
         </div>
     )
 }
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -9,23 +9,25 @@ export default function Projects() {
 
     return (
         <div className='projects' id='projects'>
-            <p className='expl'>
+            <div className='expl'>
                 <p className='title'>Our PROJECTS :</p>
-                <br/>
-                1. ACC: &nbsp;&nbsp;Design and Construction of a Competition UAV
-                <br/>
-                2. RA: &nbsp;&nbsp; Design and Construction of a Solar-Powered UAV
-                <br/>
-                3. DART: &nbsp;&nbsp;Design and Construction of a Rocket Model with Solid Propellant
-                <br/>
-                4. IT: &nbsp;&nbsp;Dealing with the Electronic and IT part of the projects, the Communication and Design Area of the Team in internal management and communication, and Taking Care of Social Media.
-            </p>
+                <br />
+                <p>
+                    1. ACC: &nbsp;&nbsp;Design and Construction of a Competition UAV
+                    <br />
+                    2. RA: &nbsp;&nbsp; Design and Construction of a Solar-Powered UAV
+                    <br />
+                    3. DART: &nbsp;&nbsp;Design and Construction of a Rocket Model with Solid Propellant
+                    <br />
+                    4. IT: &nbsp;&nbsp;Dealing with the Electronic and IT part of the projects, the Communication and Design Area of the Team in internal management and communication, and Taking Care of Social Media.
+                </p>
+            </div>
 
             <div className='gallerycontainer' >
-                <div className='box box-1' style={{ '--img': `url(${myImage1})` }} data-text="ACC"></div>
-                <div className='box box-2' style={{ '--img': `url(${myImage2})` }} data-text="DART"></div>
-                <div className='box box-3' style={{ '--img': `url(${myImage3})` }} data-text="RA"></div>
-                <div className='box box-4' style={{ '--img': `url(${myImage4})` }} data-text="IT"></div>
+                <div className='box box-1' style={{ background: `url(${myImage1}) center center` }} data-text="ACC"></div>
+                <div className='box box-2' style={{ background: `url(${myImage2}) center center` }} data-text="DART"></div>
+                <div className='box box-3' style={{ background: `url(${myImage3}) center center` }} data-text="RA"></div>
+                <div className='box box-4' style={{ background: `url(${myImage4}) center center` }} data-text="IT"></div>
             </div>
         </div>
     )
