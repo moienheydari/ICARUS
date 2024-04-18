d009edbd84edea63c2b05fb5cd12fa093de81434
Moien <137896500+moienheydari@users.noreply.github.com>
2024-04-19 00:02:30 +0200

    first page images decrease size ans style

--- src/Components/Pages/Home/ProjectsHome.jsx
+++ src/Components/Pages/Home/ProjectsHome.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../../../css/ProjectsHome.css';
 import myImage1 from '../../../Assets/ACC/ACC_Profile_1.jpg';
 import myImage2 from '../../../Assets/Dart/DART_Profile_1.jpg';
 import myImage3 from '../../../Assets/RA/RA_Profile_01.jpg';
-import myImage4 from '../../../Assets/IT/IT_Profile_01.png';
+import myImage4 from '../../../Assets/IT/IT_Profile_01.jpg';
 import { Link } from 'react-router-dom';
 
 export default function ProjectsHome() {
@@ -23,12 +23,19 @@ export default function ProjectsHome() {
                     4. IT: &nbsp;&nbsp;Dealing with the Electronic and IT part of the projects, the Communication and Design Area of the Team in internal management and communication, and Taking Care of Social Media.
                 </p>
             </div>
-
             <div className='gallerycontainer' >
-                <Link to="/ACC-Project" className='box box-1' style={{ backgroundImage: `url(${myImage1})` }} data-text="ACC"></Link>
-                <Link to="/Dart-Project" className='box box-2' style={{ backgroundImage: `url(${myImage2})` }} data-text="DART"></Link>
-                <Link to="/RA-Project" className='box box-3' style={{ backgroundImage: `url(${myImage3})` }} data-text="RA"></Link>
-                <Link to="/" className='box box-4' style={{ backgroundImage: `url(${myImage4})` }} data-text="IT"></Link>
+                <Link to="/ACC-Project" className='box box-1' data-text="ACC">
+                    <img src={myImage1} alt="not Supported" loading='eager' />
+                </Link>
+                <Link to="/Dart-Project" className='box box-2' data-text="DART">
+                    <img src={myImage2} alt="not Supported" loading='eager' />
+                </Link>
+                <Link to="/RA-Project" className='box box-3' data-text="RA">
+                    <img src={myImage3} alt="not Supported" loading='eager' />
+                </Link>
+                <Link to="/" className='box box-4' data-text="IT">
+                    <img src={myImage4} alt="not Supported" loading='eager' />
+                </Link>
             </div>
         </div>
     )
