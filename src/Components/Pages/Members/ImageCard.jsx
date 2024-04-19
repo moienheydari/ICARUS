a797cbfa5559c7cd0e645e0c5f45a728b63ba176
Moien <137896500+moienheydari@users.noreply.github.com>
2024-04-19 20:23:55 +0200

    team members done

--- src/Components/Pages/Members/ImageCard.jsx
+++ src/Components/Pages/Members/ImageCard.jsx
@@ -4,10 +4,30 @@ import '../../../css/ImageCard.css';
 export default function ImageCard({ image, index, height, name }) {
     name = new String(name)
     const nameContent = name.split('_');
+    const moreInfo = {
+        'Role': nameContent[1],
+        'Email': nameContent[4]
+    }
+    const moreInfoArray = [];
+    for (const key of Object.keys(moreInfo)) {
+        moreInfoArray.push([key, moreInfo[key]]);
+    }
 
     return (
         <div className='imagecard'>
             <img className='imagebody' src={image} alt={`image-${index}`} key={index} style={{ height: height }} />
+            <div className='info'>
+                <div className='info-text'>
+                    {moreInfoArray.map((pair) => {
+                        return (
+                            <p key={`adwdad-${pair[1]}`}>
+                                <svg width="8" height="8"><circle cx="4" cy="4" r="4" fill="white" /></svg>&nbsp;
+                                {pair[0]} : {pair[1]}<br />
+                            </p>
+                        )
+                    })}
+                </div>
+            </div>
             <p className='names'>{nameContent[2] + ' ' + nameContent[3]}</p>
         </div>
     )
--- src/Components/Pages/Members/ImageCardHead.jsx
+++ src/Components/Pages/Members/ImageCardHead.jsx
@@ -4,10 +4,28 @@ import '../../../css/ImageCardHead.css';
 export default function ImageCardHead({ image, index, name }) {
     name = new String(name)
     const nameContent = name.split('_');
+    const moreInfo = {
+    }
+    const moreInfoArray = [];
+    for (const key of Object.keys(moreInfo)) {
+        moreInfoArray.push([key, moreInfo[key]]);
+    }
 
     return (
         <div className='imagecardhead'>
-            <img className='imagehead' src={image} alt={`image-${index}`} key={index}/>
+            <img className='imagehead' src={image} alt={`image-${index}`} key={index} />
+            <div className='info'>
+                <div className='info-text'>
+                    {moreInfoArray.map((pair) => {
+                        return (
+                            <p key={`adwdad-${pair[1]}`}>
+                                <svg width="8" height="8"><circle cx="4" cy="4" r="4" fill="white" /></svg>&nbsp;
+                                {pair[0]} : {pair[1]}<br />
+                            </p>
+                        )
+                    })}
+                </div>
+            </div>
             <p className='names'>{nameContent[2] + ' ' + nameContent[3]}</p>
         </div>
     )
